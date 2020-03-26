f80193d30ea257462f5640150070f1809b0c1734
Hannah Riley <riley.hannahm@gmail.com>
2020-03-26 15:23:27 -0700

    actor routes and tests complete

---
+++ __tests__/actor.test.js
@@ -0,0 +1,56 @@
+const { getActor, getActors } = require('../db/data-helpers');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+
+describe('actor routes', () => {
+  it('creates an actor', () => {
+    return request(app)
+      .post('/api/v1/actors')
+      .send({
+        name: 'Hannah',
+        dob: '01-29-1994',
+        pob: 'Columbus'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Hannah',
+          dob: expect.any(String),
+          pob: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+  it('gets an actor by their id', async() => {
+    const actor = await getActor();
+
+    return request(app)
+      .get(`/api/v1/actors/${actor._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          ...actor
+        });
+      });
+  });
+
+  it('gets all actors', async() => {
+    const actors = await getActors();
+
+    return request(app)
+      .get('/api/v1/actors')
+      .then(res => {
+        expect(res.body).toEqual(actors);
+        // expect(res.body).toHaveLength(3);
+        // tweets.forEach(tweet => {
+        //   expect(res.body).toContainEqual({
+        //     ...tweet.toJSON(),
+        //     _id: tweet.id
+        //   });
+        // });
+      });
+  });
+
+});
--- db/seed.js
+++ db/seed.js
@@ -1,14 +1,21 @@
 const Studio = require('../lib/models/Studio');
+const Actor = require('../lib/models/Actor');
 
 const chance = require('chance').Chance();
 
 
-module.exports = async({ studiosToCreate = 10 } = {}) => {
+module.exports = async({ studiosToCreate = 10, actorsToCreate = 10 } = {}) => {
 
 
   const studios = await Studio.create([...Array(studiosToCreate)].map(() => ({
     name: chance.name(),
     address: chance.sentence()
+  })));
+
+  const actors = await Actor.create([...Array(actorsToCreate)].map(() => ({
+    name: chance.name(),
+    dob: chance.date({ string: true }),
+    pob: chance.city()
   })));
 
 

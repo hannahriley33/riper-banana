8dad72c67dfce410e035b59dff595c3687117af3
Hannah Riley <riley.hannahm@gmail.com>
2020-03-26 15:02:16 -0700

    studio routes and tests complete

---
+++ __tests__/studio.test.js
@@ -0,0 +1,62 @@
+const { getStudio, getStudios } = require('../db/data-helpers');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+
+describe('studio routes', () => {
+  it('creates a studio', () => {
+    return request(app)
+      .post('/api/v1/studios')
+      .send({
+        name: 'Hannah',
+        address: {
+          city:  'Palo Alto',
+          state: 'CA',
+          country: 'USA'
+        }
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Hannah',
+          address: {
+            city:  'Palo Alto',
+            state: 'CA',
+            country: 'USA'
+          },
+          __v: 0
+        });
+      });
+  });
+
+  it('gets a studio by id', async() => {
+    const studio = await getStudio();
+
+    return request(app)
+      .get(`/api/v1/studios/${studio._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          ...studio
+        });
+      });
+  });
+
+  it('gets all studios', async() => {
+    const studios = await getStudios();
+
+    return request(app)
+      .get('/api/v1/studios')
+      .then(res => {
+        expect(res.body).toEqual(studios);
+        // expect(res.body).toHaveLength(3);
+        // tweets.forEach(tweet => {
+        //   expect(res.body).toContainEqual({
+        //     ...tweet.toJSON(),
+        //     _id: tweet.id
+        //   });
+        // });
+      });
+  });
+
+});
--- db/seed.js
+++ db/seed.js
@@ -1,22 +1,15 @@
 const Studio = require('../lib/models/Studio');
 
+const chance = require('chance').Chance();
 
-// specifying the number of tweets to create with our seed function
-module.exports = async({ tweetsToCreate = 10, commentsToCreate = 100 } = {}) => {
-  // creating tweets
-  // creating an array of tweetsToCreate length
-  // map through the array
-  // -> for each item in the array we create an object with { handle, text }
-  // for each tweet in the mapped array we create a tweet in our mongodb
-  const handles = ['@spot', '@rover', '@bingo'];
-  const tweets = await Tweet.create([...Array(tweetsToCreate)].map(() => ({
-    handle: chance.pickone(handles),
-    text: chance.sentence()
+
+module.exports = async({ studiosToCreate = 10 } = {}) => {
+
+
+  const studios = await Studio.create([...Array(studiosToCreate)].map(() => ({
+    name: chance.name(),
+    address: chance.sentence()
   })));
 
-  await Comment.create([...Array(commentsToCreate)].map(() => ({
-    tweetId: chance.pickone(tweets)._id,
-    handle: chance.pickone(handles),
-    text: chance.sentence()
-  })));
+
 };
